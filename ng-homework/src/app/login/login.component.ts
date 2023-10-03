28edcccdcea597ead6dae451c49a151ae9370be8
GioAbuladze-Creator <giorgi.abuladze.1@btu.edu.ge>
2023-10-03 22:48:31 +0400

    login checks user and finds

--- ng-homework/src/app/login/login.component.ts
+++ ng-homework/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UsersService } from '../homework1/shared/users.service';
+import { User } from '../homework1/shared/user.interface';
 
 declare var window:any;
 
@@ -10,13 +12,17 @@ declare var window:any;
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit{
+  logged:boolean=false;
+  user!:User;
+  invalidLogin:boolean=false;
   constructor(
     private fb:FormBuilder,
+    private usersService:UsersService
   ){}
 
   form=this.fb.group({
     email:['',[Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]],
-    password:['', [Validators.required, Validators.pattern(/^[A-Za-z0-9]{7,}$/)]]
+    password:['', [Validators.required]]
   })
   get email(){
     return this.form.get('email')
@@ -36,5 +42,18 @@ export class LoginComponent implements OnInit{
   closeModal(){
     this.formModal.hide()
   }
-  onLogIn(){}
+  
+  onLogIn(){
+    let found=this.usersService.userList.find((user)=>{return user.email==this.email?.value&&user.password==this.password?.value})
+    if(found){
+      this.invalidLogin=false;
+      this.logged=true;
+      this.user=found;
+      console.log(found)
+      
+    }else{
+      this.invalidLogin=true;
+      console.log(found)
+    }
+  }
 }
